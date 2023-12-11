6eedbf2a48607ef844810fc6b730c74b5b9252d2
Hachounet <hachounet.github@gmx.fr>
2023-12-11 15:07:44 +0100

    Pre-final EASketch
    need to work on buttons style when activated/desactived

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const rainbowColor = generateRainbowColor();
 let currentMode = 0;
 let inputGridSize = 16;
 let originalBtnRainbowText = "Rainbow mode !"
+let r = 255
+let g = 255
+let b = 255
+let steps = 255/10
+let stepsRemaining = 10;
 
 // Body parts
 const body = document.getElementById('body')
@@ -39,7 +44,7 @@ document.body.insertBefore(header, document.body.firstChild);
 function creatingGrid () {
     myGrid.innerHTML = "";
 for (let x=0; x < inputGridSize * inputGridSize; x++) {
-    var caseOfGrid = document.createElement('div')
+    let caseOfGrid = document.createElement('div')
     caseOfGrid.className = "case";
     myGrid.appendChild(caseOfGrid)
 }
@@ -62,13 +67,17 @@ document.addEventListener('mouseup', function() {
     isMouseDown = false;
 });
 
-myGrid.addEventListener("mousemove", function(event) {
+myGrid.addEventListener("mouseover", function(event) {
     if ( isMouseDown && event.target.classList.contains('case')){
         if ( currentMode === 0){
         event.target.style.backgroundColor= "darkslategray";}
         else if ( currentMode === 1){
         event.target.style.backgroundColor= generateRainbowColor();
             }
+        else if (currentMode === 2) {
+        event.target.style.backgroundColor = incrementColor();
+            }
+        
         }
     }
 );
@@ -89,7 +98,7 @@ btnSizeGrid.addEventListener("click", changeGridSize)
 function changeGridSize () {
    inputGridSize = parseInt(prompt("Choose your grid size ! Should be a maximum of 100."));
     if ( isNaN(inputGridSize) || inputGridSize > 100 || inputGridSize < 0 ){
-        prompt("Choose a correct size.")
+        prompt("Choose a correct size, must be a number, between 1 and 100.")
     }
     else {
         document.documentElement.style.setProperty('--inputUserSize', inputGridSize)
@@ -108,11 +117,13 @@ const btnColor = document.createElement('button')
     btnColor.id = 'btnColor'
     btnColor.classList.add ("buttons")
 document.getElementById('usermenu').appendChild(btnColor);
-btnColor.addEventListener("click", switchRainbowButton)
+btnColor.addEventListener("click", switchRainbowLed)
 
-function switchRainbowButton () {
+function switchRainbowLed () {
     currentMode = 0;
     btnRainbow.innerHTML = originalBtnRainbowText;
+    btnRainbow.classList.add('buttons')
+    btnRainbow.classList.remove('top-round-rainbow')
     header.style.backgroundColor = "linen";
 }
 
@@ -126,16 +137,13 @@ const btnRainbow = document.createElement('button')
     
 document.getElementById('usermenu').appendChild(btnRainbow);
 btnRainbow.addEventListener("click", toggleRainbowMode)
-btnRainbow.addEventListener("click", toggleRainbowLed)
 
+
+// switch currentmode for correct choice in Hover
 function toggleRainbowMode () {
 
     currentMode = 1;
-}
-
-function toggleRainbowLed () {
-    if ( currentMode === 1) {
-        const btnRainbow = document.getElementById('btnRainbow')
+    const btnRainbow = document.getElementById('btnRainbow')
         const rainbowText = btnRainbow.textContent
         let coloredText= ``;
         for ( let i = 0; i < rainbowText.length; i++){
@@ -146,14 +154,6 @@ function toggleRainbowLed () {
         btnRainbow.classList.remove('buttons')
         btnRainbow.classList.add('top-round-rainbow')
         header.style.backgroundColor = generateRainbowColor();
-
-        
-    }
-    else if ( currentMode === 0) {
-        btnRainbow.innerHTML = originalBtnRainbowText;
-        header.style.backgroundColor = "linen";
-        
-    }
 }
 
 // User menu - Gradiant button
@@ -164,16 +164,78 @@ const btnGradiant = document.createElement('button')
     btnGradiant.id="btnGradiant"
 
 document.getElementById('usermenu').appendChild(btnGradiant)
-btnGradiant.addEventListener('click', incrementColor)
+btnGradiant.addEventListener("click", switchGradiantMode );
 
-function incrementColor(color, steps) {
-    const stepSize = 255 / steps;
-    const r = Math.min(color.r + stepSize, 255);
-    const g = Math.min(color.g + stepSize, 255);
-    const b = Math.min(color.b + stepSize, 255);
-
-    return { r, g, b };
+// Reset r, g, b, steps and stepsRemaining to be sure of good fonctionnement of incrementColor function if already used
+function switchGradiantMode() {
+    currentMode = 2;
+    document.getElementById('btnGradiant').style.background = 'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(255,255,255,1) 100%)';
+    resetGradientState();
 }
+
+function resetGradientState() {
+    r = 255;
+    g = 255;
+    b = 255;
+    steps = 255 / 10;
+    stepsRemaining = 10;
+    uppingDowning = 1;
+}
+
+let uppingDowning = 1;
+function incrementColor () {
+
+if ( uppingDowning === 1){
+    r = Math.max(0, Math.round(r - steps));
+    g = Math.max(0, Math.round(g - steps));
+    b = Math.max(0, Math.round(b - steps));
+
+    const hexColor = rgbToHex(r, g, b)
+    stepsRemaining--;
+    if ( stepsRemaining === 0 ) {
+        uppingDowning = 0;
+    }
+    console.log(`Je réduis r g b jusqu'à 0 : ${r}   ${g}   ${b}`);
+    console.log(`La valeur de stepsRemaining est de ${stepsRemaining}`)
+    return hexColor
+
+    
+
+}
+
+else if ( uppingDowning === 0) {
+    steps = 255/10
+        r = Math.min(255, Math.round(r + steps));
+        g = Math.min(255, Math.round(g + steps));
+        b = Math.min(255, Math.round(b + steps));
+
+        const hexColor = rgbToHex(r, g, b)
+        stepsRemaining++;
+        if ( stepsRemaining === 10) {
+            uppingDowning = 1;
+        }
+        console.log(`J'augmente r g b jusqu'à 255 : ${r}   ${g}   ${b}`);
+        console.log(`La valeur de stepsRemaining est de ${stepsRemaining}`)
+        return hexColor
+       
+    }
+}
+
+// Function to convert RGB to HEX
+
+function componentToHex(c) {
+    const hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+  
+  function rgbToHex(r, g, b) {
+    const hexR = componentToHex(r);
+    const hexG = componentToHex(g);
+    const hexB = componentToHex(b);
+
+    return `#${hexR}${hexG}${hexB}`;
+  }
+
 
 // User menu - Button reset 
 
